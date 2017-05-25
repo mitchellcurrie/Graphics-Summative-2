d7914beb41c61e9b49eb5544e0eea4ee91504abd
MDS\mitchell.cur6577 <mitchell.currie@hotmail.com>
2017-05-26 08:58:56 +1200

    Add Spotlight

--- Graphics Summative 2/assets/shaders/specular.fs
+++ Graphics Summative 2/assets/shaders/specular.fs
@@ -16,32 +16,66 @@ objectColor,
 lightPosition,
 viewPosition;
 
+
+//vec3 ApplyLight(int lightType);
+
 void main() {
 
-    // Ambient light
-    vec3 ambientValue = ambientStrength * lightColor;
+    for (int x = 0; x < 2; x++)
+    { 
+        float attenuation = 1.0f;
 
-    // Diffuse light
-    vec3 normal = normalize(OutNormVec);
-    vec3 lightDir = normalize(lightPosition - OutFragPosition);
+//        if (x == 1)
+//        {
+//            lightColor = vec(1.0f, 0.0f, 0.0f);
+//        }
 
-    float difference = max(dot(normal, lightDir), 0.0f);
-    vec3 diffuseValue = difference * lightColor;
+        // Ambient light
+        vec3 ambientValue = ambientStrength * lightColor;
+
+        // Diffuse light
+        vec3 normal = normalize(OutNormVec);
+        vec3 lightDir = normalize(lightPosition - OutFragPosition);
+
+        float difference = max(dot(normal, lightDir), 0.0f);
+        vec3 diffuseValue = difference * lightColor;
      
-    // Specular light
-    vec3 viewDir = normalize(viewPosition - OutFragPosition);
-    vec3 reflectionDir = reflect(-lightDir, normal);
-    //float spec = pow(max(dot(viewDir, reflectionDir), 0.0f), 32);
+        // Specular light
+        vec3 viewDir = normalize(viewPosition - OutFragPosition);
+        vec3 reflectionDir = reflect(-lightDir, normal);
+        //float spec = pow(max(dot(viewDir, reflectionDir), 0.0f), 32);
     
-    // Gouraud Shader
-    vec3 halfVector = normalize(lightDir + viewDir);
+        // Gouraud Shader
+        vec3 halfVector = normalize(lightDir + viewDir);
 
-    float spec = pow(max(dot(normal, halfVector), 0.0f), 32);
-    vec3 specularValue = spec * lightColor * specularStrength;
+        float spec = pow(max(dot(normal, halfVector), 0.0f), 32);
+        vec3 specularValue = spec * lightColor * specularStrength;
 
-    // Final values
-    vec3 finalObjectLight = (ambientValue + diffuseValue + specularValue) * objectColor;
 
-    color = texture(Texture, TexCoord) * vec4(finalObjectLight,  1.0f);
+        ////// NEW ////
 
+        if (x == 1) // spotlight
+        {
+            vec3 coneDirection = vec3(1.0f, 0.0f, 0.0f);
+            float coneAngle = 10.0f; // degrees
+
+            float lightToSurfaceAngle = degrees(acos(dot(lightDir, normalize(coneDirection))));
+
+            if(lightToSurfaceAngle > coneAngle)
+            {
+                attenuation = 0.0f;
+            }
+        }
+
+        ////// END NEW //////
+
+        // Final values
+        vec3 finalObjectLight = (ambientValue + diffuseValue + specularValue) * objectColor * attenuation;
+
+        color += texture(Texture, TexCoord) * vec4(finalObjectLight,  1.0f);
+    }
+
+ //   color = texture(Texture, TexCoord) * vec4(finalObjectLight,  1.0f);
+
+ //   return finalObjectLight;
 }
